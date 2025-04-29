a71569e1904d3050fb58c8ea09b005f013203909
ROUNAK RAJ <rajrounak366@gmail.com>
2025-04-29 16:22:29 +0530

    Update Movie Recommendation_app.py

--- Movie Recommendation_app.py
+++ Movie Recommendation_app.py
@@ -1,53 +1,61 @@
 import pandas as pd
-from joblib import dump,load
 import numpy as np
-import streamlit as st
 import requests
+import streamlit as st
+from joblib import load
+
+# ------------------ Utility Function to Fetch Poster ------------------ #
 def fetch_poster(movie_id):
-    url="https://api.themoviedb.org/3/movie/{}?api_key=c496e4f6892c1c41cbb4877b52db7c73&language=en-US".format(movie_id)
-    data=requests.get(url)
-    data=data.json()
-    st.text(data)
-    st.text("https://api.themoviedb.org/3/movie/{}?api_key=c496e4f6892c1c41cbb4877b52db7c73&language=en-US".format(movie_id))
-    poster_path= data['poster_path']
-    full_path="https://image.tmdb.org/t/p/w500/"+ poster_path
-    return full_path
-def recommend(movie):
-    movie_index=movies[movies['title']==movie].index[0]
-    distances=similarity[movie_index]
-    movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
-    recommended_movies_name=[]
-    recommended_movies_poster=[]
-    for i in movies_list:
-        movie_id=movies.iloc[i[0]].movie_id
-        recommended_movies_name.append(movies.iloc[i[0]].title)
-        recommended_movies_poster.append(fetch_poster(movie_id))
-    return recommended_movies_name,recommended_movies_poster
-movies=load('movies.joblib')
-similarity=load('similarity.joblib')
-movies=pd.DataFrame(movies)
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=c496e4f6892c1c41cbb4877b52db7c73&language=en-US"
+    response = requests.get(url)
+    data = response.json()
 
-st.title('Movie Recommendation System')
-selected_movie_name=st.selectbox('Type to select your movie',movies['title'].values)
+    if 'poster_path' in data and data['poster_path']:
+        return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+    else:
+        return "https://via.placeholder.com/500x750?text=No+Image"
 
-if st.button('Recommend'):
-    recommended_movies_name,recommended_movies_poster=recommend(selected_movie_name)
-    col1,col2,col3,col4,col5=st.columns(5)
-    with col1:
-        st.text(recommended_movies_name[0])
-        st.image(recommended_movies_poster[0])
-    with col2:
-        st.text(recommended_movies_name[1])
-        st.image(recommended_movies_poster[1])
-    with col3:
-        st.text(recommended_movies_name[2])
-        st.image(recommended_movies_poster[2])
-    with col4:
-        st.text(recommended_movies_name[3])
-        st.image(recommended_movies_poster[3])
-    with col5:
-        st.text(recommended_movies_name[4])
-        st.image(recommended_movies_poster[4])
+# ------------------ Recommendation Function ------------------ #
+def recommend(movie_title):
+    try:
+        index = movies[movies['title'] == movie_title].index[0]
+    except IndexError:
+        st.error("Selected movie not found in database.")
+        return [], []
 
-# for i in recommendation:
-   #     st.write(i)
+    distances = sorted(
+        list(enumerate(similarity[index])),
+        reverse=True,
+        key=lambda x: x[1]
+    )
+
+    recommended_movies = []
+    recommended_posters = []
+
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]]['movie_id']
+        recommended_movies.append(movies.iloc[i[0]]['title'])
+        recommended_posters.append(fetch_poster(movie_id))
+
+    return recommended_movies, recommended_posters
+
+# ------------------ Streamlit App UI ------------------ #
+st.header('🎬 Movie Recommendation System')
+
+# Load data
+movies = pd.DataFrame(load('movie.joblib'))
+similarity = load('similarity.joblib')
+
+# Dropdown for movie selection
+movie_list = movies['title'].values
+selected_movie = st.selectbox('Type to select a movie from dropdown:', movie_list)
+
+# Show recommendations
+if st.button('Show Recommendation'):
+    names, posters = recommend(selected_movie)
+    if names and posters:
+        columns = st.columns(5)
+        for i in range(5):
+            with columns[i]:
+                st.text(names[i])
+                st.image(posters[i])
